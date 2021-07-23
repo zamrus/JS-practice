02403d016927d831574bf8c65bb21f737f449ab6
Ruslan <northernnavy95@gmail.com>
2021-07-23 14:07:18 +0300

    adding simple function methods

--- functions.js
+++ functions.js
@@ -23,3 +23,27 @@ let newGenerator = generator();
 for (let i = 0; i <= 3; i++) {
     console.log(newGenerator.next());
 }
+
+// recursion
+
+const factorial = (num) => {
+    if(num == 1) {
+        return num;
+    }
+    return num * factorial(num - 1);
+}
+
+let res = factorial(5);
+
+console.log(res);
+
+// closure 
+
+const closureFunc = (c) => {
+    return function(a, b) {
+        return a + b + c;
+    }
+}
+
+let childFunc = closureFunc(5);
+console.log(childFunc(2, 3));
